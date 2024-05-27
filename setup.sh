05988c5585b94aad9f0b12a75546c2782e701597
Tanya2544 <93580162+TanyaratJivnot@users.noreply.github.com>
2024-05-28 01:40:59 +0700

    Add setuptools to requirements.txt

--- setup.sh
+++ setup.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 apt-get update
 apt-get install -y python3-distutils
+curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
+python3 get-pip.py
 pip install --upgrade setuptools
 pip install --upgrade -r requirements.txt
